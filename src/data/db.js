4c5078ea4a3397fcb303e2276ba120ab51fc116e
AlexTraveylan <monsieurdemares@gmail.com>
2023-03-01 23:49:51 +0100

    autre essai

--- src/data/db.js
+++ src/data/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const db_adress = process.env.REACT_APP_DB_ADDRESS;
+const db_adress = process.env.MONGODB_URI;
 
 const connectDb = (handler) => async (req, res) => {
     if (mongoose.connections[0].readyState) {
